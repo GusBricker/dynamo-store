d6e1785c812ef2f33cd89a7aba0e32cff85f281c
Chris Lapa <chris@lapa.com.au>
2018-08-22 08:22:31 +1000

    object: tests: adds several more complicated test cases.
    Signed-off-by: Chris Lapa <chris@lapa.com.au>

--- tests/test_object.py
+++ tests/test_object.py
@@ -212,3 +212,155 @@ def test_can_guess_objects_with_internal_loader(root_store, base_item):
     assert o.birth_details.hospital == 'Kosei Nenkin'
     assert o.__primary_key == test_pk
 
+class BaseMultipleLocation(DyObject):
+    TABLE_NAME = 'DynamoStoreRootDB'
+    REGION_NAME = 'us-east-2'
+    PRIMARY_KEY_NAME = 'ID'
+    IGNORE_LIST = ['address']
+    firstname = fields.StringField()
+    lastname = fields.StringField()
+    locations = fields.ListField([Location])
+    birth_details = fields.EmbeddedField(BirthDetails, default=BirthDetails())
+    address = None
+
+def loader3(config, data):
+    print(config)
+    if config == DyStore.CONFIG_LOADER_LOAD_KEY:
+        encrypted_paths = ['birth_details.hospital', 'birth_details.dob', 'location.city', 'location.country', 'firstname', 'lastname']
+        if data in encrypted_paths:
+            return '123kgk132l'
+    elif config == DyStore.CONFIG_LOADER_KEEP_METADATA:
+        return False
+
+    return None
+
+def test_can_write_read_multisharded_object(root_store, base_item):
+    orig = BaseMultipleLocation(birth_details=BirthDetails())
+    orig.address = '123 fake st'
+    orig.firstname = 'john'
+    orig.lastname = 'smith'
+
+    loc1 = Location(geolocation=GeoLocation())
+    loc1.city = 'Vienna'
+    loc1.country = 'Austria'
+    loc1.geolocation.lattitude = '40.0'
+    loc1.geolocation.longitude = '41.0'
+    orig.locations.append(loc1)
+
+    loc2 = Location(geolocation=GeoLocation())
+    loc2.city = 'Bejing'
+    loc2.country = 'China'
+    loc2.geolocation.lattitude = '50.0'
+    loc2.geolocation.longitude = '51.0'
+    orig.locations.append(loc2)
+
+    orig.birth_details.dob = '15/03/1980'
+    orig.birth_details.hospital = 'Good one'
+    key = orig.save(config_loader=loader3)
+    assert orig.__primary_key == key
+
+    o = BaseMultipleLocation.load(key, config_loader=loader3)
+    assert o.address == None
+    assert o.firstname == 'john'
+    assert o.lastname == 'smith'
+
+    assert isinstance(o.locations, list)
+    assert len(o.locations) == 2
+    assert o.locations[0].city == 'Vienna'
+    assert o.locations[0].country == 'Austria'
+    assert isinstance(o.locations[0].geolocation, GeoLocation)
+    assert o.locations[0].geolocation.lattitude == '40.0'
+    assert o.locations[0].geolocation.longitude == '41.0'
+
+    assert o.locations[1].city == 'Bejing'
+    assert o.locations[1].country == 'China'
+    assert isinstance(o.locations[1].geolocation, GeoLocation)
+    assert o.locations[1].geolocation.lattitude == '50.0'
+    assert o.locations[1].geolocation.longitude == '51.0'
+
+    assert isinstance(o.birth_details, BirthDetails)
+    assert o.birth_details.dob == '15/03/1980'
+    assert o.birth_details.hospital == 'Good one'
+    assert o.__primary_key == key
+
+class NotAShard(DyObject):
+    line = fields.IntField()
+    data = fields.StringField()
+
+class BaseMixedShards(DyObject):
+    TABLE_NAME = 'DynamoStoreRootDB'
+    REGION_NAME = 'us-east-2'
+    PRIMARY_KEY_NAME = 'ID'
+    IGNORE_LIST = ['address']
+    firstname = fields.StringField()
+    lastname = fields.StringField()
+    locations = fields.ListField([Location])
+    birth_details = fields.EmbeddedField(BirthDetails, default=BirthDetails())
+    info = fields.EmbeddedField(NotAShard, default=NotAShard())
+    address = None
+
+def loader4(config, data):
+    print(config)
+    if config == DyStore.CONFIG_LOADER_LOAD_KEY:
+        encrypted_paths = ['birth_details.hospital', 'birth_details.dob', 'location.city', 'location.country',
+                            'firstname', 'lastname', 'info.line', 'info.data']
+        if data in encrypted_paths:
+            return '123kgk132l'
+    elif config == DyStore.CONFIG_LOADER_KEEP_METADATA:
+        return False
+
+    return None
+
+def test_can_read_write_mixed_shard(root_store, base_item):
+    orig = BaseMixedShards(info=NotAShard(), birth_details=BirthDetails())
+    orig.address = '123 fake st'
+    orig.firstname = 'john'
+    orig.lastname = 'smith'
+    orig.info.line = 100
+    orig.info.data = 'line data'
+
+    loc1 = Location(geolocation=GeoLocation())
+    loc1.city = 'Vienna'
+    loc1.country = 'Austria'
+    loc1.geolocation.lattitude = '40.0'
+    loc1.geolocation.longitude = '41.0'
+    orig.locations.append(loc1)
+
+    loc2 = Location(geolocation=GeoLocation())
+    loc2.city = 'Bejing'
+    loc2.country = 'China'
+    loc2.geolocation.lattitude = '50.0'
+    loc2.geolocation.longitude = '51.0'
+    orig.locations.append(loc2)
+
+    orig.birth_details.dob = '15/03/1980'
+    orig.birth_details.hospital = 'Good one'
+    key = orig.save(config_loader=loader4)
+    assert orig.__primary_key == key
+
+    o = BaseMixedShards.load(key, config_loader=loader4)
+    assert o.address == None
+    assert o.firstname == 'john'
+    assert o.lastname == 'smith'
+    assert isinstance(o.info, NotAShard)
+    assert orig.info.line == 100
+    assert orig.info.data == 'line data'
+
+    assert isinstance(o.locations, list)
+    assert len(o.locations) == 2
+    assert o.locations[0].city == 'Vienna'
+    assert o.locations[0].country == 'Austria'
+    assert isinstance(o.locations[0].geolocation, GeoLocation)
+    assert o.locations[0].geolocation.lattitude == '40.0'
+    assert o.locations[0].geolocation.longitude == '41.0'
+
+    assert o.locations[1].city == 'Bejing'
+    assert o.locations[1].country == 'China'
+    assert isinstance(o.locations[1].geolocation, GeoLocation)
+    assert o.locations[1].geolocation.lattitude == '50.0'
+    assert o.locations[1].geolocation.longitude == '51.0'
+
+    assert isinstance(o.birth_details, BirthDetails)
+    assert o.birth_details.dob == '15/03/1980'
+    assert o.birth_details.hospital == 'Good one'
+    assert o.__primary_key == key
