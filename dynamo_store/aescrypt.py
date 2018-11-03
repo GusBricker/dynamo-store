c91673cc7587df96f0068682822b2bfd5fb85d53
Chris Lapa <chris@lapa.com.au>
2018-11-04 00:03:24 +1100

    crypto: using base64 due to crash when string shorter then IV.
    Signed-off-by: Chris Lapa <chris@lapa.com.au>

--- dynamo_store/aescrypt.py
+++ dynamo_store/aescrypt.py
@@ -1,7 +1,9 @@
 from boto3.dynamodb.types import Binary
 import hashlib
+import base64
 from Crypto import Random
 from Crypto.Cipher import AES
+from dynamo_store.log import logger
 
 class AESCipher(object):
 
@@ -13,19 +15,23 @@ class AESCipher(object):
         raw = self._pad(raw)
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return iv + cipher.encrypt(raw)
+        enc = base64.b64encode(iv + cipher.encrypt(raw))
+        logger.debug('Encrypted item: %s > %s' % (raw, enc.decode('utf-8')))
+        return enc
 
-    def decrypt(self, enc):
-        if isinstance(enc, Binary):
-            enc = enc.value
+    def decrypt(self, enc1):
+        if isinstance(enc1, Binary):
+            enc1 = enc1.value
 
         if not enc1:
             return enc1
 
+        enc = base64.b64decode(enc1)
         iv = enc[:AES.block_size]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         data = self._unpad(cipher.decrypt(enc[AES.block_size:]))
         data = data.decode('utf-8')
+        logger.debug('Decrypted item: %s > %s' % (enc1.decode('utf-8'), data))
         return data
 
     def _pad(self, s):
